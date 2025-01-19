137d9cbac9f78a7b85a323443e98165aa19e125b
kuro2907 <nam.le@visily.ai>
2025-01-20 01:16:48 +0700

    feat: code baseline sampling

--- main_testbed.py
+++ main_testbed.py
@@ -1,23 +1,23 @@
 import os
-import sys 
+import sys
 import torch
-from tqdm import tqdm 
+from tqdm import tqdm
 import glob
 import pandas as pd
 import numpy as np
 import argparse
-import time   
-import timm 
-import yaml 
-
+import time
+import timm
+import yaml
+from testbed.scoring_sampling import sampling_method_
 import openslide
 
 from data.merge_dataset import SuperpixelDataset, PatchDataset
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from PIL import Image
-from patch_merging import tome 
-from utils import utils  
+from patch_merging import tome
+from utils import utils
 from testbed.importance_scores import get_scoring_do_nothing
 from testbed.pruning import get_pruning_do_nothing
 
@@ -28,120 +28,158 @@ SCORING_FUNCTION_MAP = {
 
 PRUNING_FUNCTION_MAP = {
     "get_pruning_do_nothing": get_pruning_do_nothing,
-}   
+}
+
 
 def load_config(config_file):
     # Load configuration from the provided YAML file
-    with open(config_file, 'r') as f:
+    with open(config_file, "r") as f:
         config = yaml.safe_load(f)
     return config
 
 
-
-PROJECT_DIR = os.environ.get('PROJECT_DIR')
-sys.path.append(os.path.join(PROJECT_DIR))  
+PROJECT_DIR = os.environ.get("/Users/nam.le/Desktop/research/camil_pytorch")
+sys.path.append(PROJECT_DIR)
 
 # SLIDE_DIR = '/project/hnguyen2/hqvo3/Datasets/digital_pathology/public/CAMELYON16'
-example_list = ['normal_072', 'normal_001', 'normal_048', 'tumor_026', 'tumor_031', 'tumor_032']
+example_list = [
+    "normal_072",
+    "normal_001",
+    "normal_048",
+    "tumor_026",
+    "tumor_031",
+    "tumor_032",
+]
 
-model = timm.create_model('vit_base_patch16_224', pretrained=True)  # You can choose any model
-model.eval()  
+model = timm.create_model(
+    "vit_base_patch16_224", pretrained=True
+)  # You can choose any model
+model.eval()
+
 
 def main(args):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),  # Resize the patch to 256x256
-        transforms.ToTensor(),          # Convert the image to a PyTorch tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize with ImageNet stats
-        # You can add other transformations like RandomHorizontalFlip, RandomRotation, etc.
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),  # Resize the patch to 256x256
+            transforms.ToTensor(),  # Convert the image to a PyTorch tensor
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+            ),  # Normalize with ImageNet stats
+            # You can add other transformations like RandomHorizontalFlip, RandomRotation, etc.
+        ]
+    )
     if args.dry_run:
         print("Running the dry run")
     else:
         print("Running on full data")
     start_slide = time.time()
-    
-    wsi_paths = glob.glob(os.path.join(args.slide_path, '*.tif'))
-    wsi_paths = [path for path in wsi_paths if os.path.basename(path).split(".")[0] in example_list]
+
+    wsi_paths = glob.glob(os.path.join(args.slide_path, "*.tif"))
+    wsi_paths = [
+        path
+        for path in wsi_paths
+        if os.path.basename(path).split(".")[0] in example_list
+    ]
     json_folder = args.json_path
-    
+
     superpixel_dataset = SuperpixelDataset(
         slide_paths=wsi_paths,
         json_folder=json_folder,
-        )
-    print("Number of slide in dataset:", len(superpixel_dataset)) 
-    
+    )
+    print("Number of slide in dataset:", len(superpixel_dataset))
+
     for slide_index in range(len(superpixel_dataset)):
         superpixel_datas, wsi_path = superpixel_dataset[slide_index]
         print(wsi_path)
-        #slide = openslide.open_slide(wsi_path)  
+        # slide = openslide.open_slide(wsi_path)
         print(len(superpixel_datas))
+        superpixel_datas = superpixel_datas[:2]
         slide_basename = os.path.basename(wsi_path).split(".")[0]
         print("Basename:", slide_basename)
-        save_dir = os.path.join(args.spixel_path, slide_basename) 
+        save_dir = os.path.join(args.spixel_path, slide_basename)
         start_slide = time.time()
+
+        store_array = []
+
         for each_superpixel in superpixel_datas:
             start_spixel = time.time()
-            foreground_idx = each_superpixel['foreground_idx'] 
-            xywh_abs_bbox = each_superpixel['xywh_abs_bbox']
-            superpixel_extrapolated = each_superpixel['superpixel_extrapolated']
-            
+            foreground_idx = each_superpixel["foreground_idx"]
+            xywh_abs_bbox = each_superpixel["xywh_abs_bbox"]
+            superpixel_extrapolated = each_superpixel["superpixel_extrapolated"]
+
             start_spixel = time.time()
-            
+
             superpixel_np = utils.read_region_from_npy(
-                args.spixel_path, 
-                slide_basename, 
-                foreground_idx
-                )
-            print("- Complete reading after: ", time.time()-start_spixel)
-            
+                args.spixel_path, slide_basename, foreground_idx
+            )
+            print("- Complete reading after: ", time.time() - start_spixel)
+
             patch_dataset = PatchDataset(
                 superpixel_np,
-                superpixel_extrapolated, 
+                superpixel_extrapolated,
                 patch_size=(224, 224),
                 transform=transform,
                 coverage_threshold=0.5,
                 return_feature=True,  # Enable feature extraction
-                model=model
-            ) 
-            patch_dataloader = DataLoader(patch_dataset, batch_size=args.batch_size, shuffle=False) 
-            
+                model=model,
+            )
+            patch_dataloader = DataLoader(
+                patch_dataset, batch_size=args.batch_size, shuffle=False
+            )
+
             _all_features_spixel = []
             _all_idxes_spixel = []
-            
-            for batch_features, batch_patches, batch_bboxes, batch_idxes in tqdm(patch_dataloader):
+
+            for batch_features, batch_patches, batch_bboxes, batch_idxes in tqdm(
+                patch_dataloader
+            ):
                 _flatten_features = batch_features.view(-1, batch_features.shape[-1])
                 _all_features_spixel.append(_flatten_features)
                 _all_idxes_spixel.append(batch_idxes)
-                
-            spixel_patch_features = torch.cat(_all_features_spixel)  # of a 
-            print(f"Final feature shape for superpixel {foreground_idx}: {spixel_patch_features.shape})")
-            print("Complete processing a superpixel after :", time.time()-start_spixel)
-        print('Complete an Slide after: ', time.time()-start_slide)
+
+            spixel_patch_features = torch.cat(_all_features_spixel)  # of a
+            # print("Tensor details:")
+            # print("Shape:", spixel_patch_features.shape)
+            # print("Data type:", spixel_patch_features.dtype)
+            # print("Device:", spixel_patch_features.device)
+            # spixel_patch_features = sampling_method_(spixel_patch_features)
+            store_array.append(spixel_patch_features)
+            output_tensor_check = torch.cat(store_array, dim=0)
+            print("Shape:", output_tensor_check.shape)
+            # Print all elements of the tensor
+            print("Elements:")
+            print(spixel_patch_features)
+            # scoring - sampling
+            # add classifier (SSL) - input spixel_patch_features
+            print(
+                f"Final feature shape for superpixel {foreground_idx}: {spixel_patch_features.shape})"
+            )
+            print(
+                "Complete processing a superpixel after :", time.time() - start_spixel
+            )
+        output_tensor_final = torch.cat((store_array), dim=0)
+        output_tensor_final = sampling_method_(output_tensor_final)
+        print("Complete an Slide after: ", time.time() - start_slide)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dry_run', type=bool, default=False)
-    parser.add_argument('--config_file', default='ma_exp001')
+    parser.add_argument("--dry_run", type=bool, default=False)
+    parser.add_argument("--config_file", default="ma_exp001")
     args = parser.parse_args()
-    
-    if os.path.exists(f'./testbest_config/{args.config_file}.yaml'):
-        config = load_config(f'./testbest_config/{args.config_file}.yaml')
-        args.use_features = config.get('use_features', True)
-        
-        args.slide_path = config.get('SLIDE_PATH')
-        args.json_path = config.get('JSON_PATH')
-        args.spixel_path = config.get('SPIXEL_PATH')
-        
-        args.scoring_function = SCORING_FUNCTION_MAP.get(
-            config.get("scoring_function")
-        )
-        args.pruning_function = PRUNING_FUNCTION_MAP.get(
-            config.get('pruning_function') 
-        )
-        args.batch_size = config.get('batch_size')
+
+    if os.path.exists(f"./testbest_config/{args.config_file}.yaml"):
+        config = load_config(f"./testbest_config/{args.config_file}.yaml")
+        args.use_features = config.get("use_features", True)
+
+        args.slide_path = config.get("SLIDE_PATH")
+        args.json_path = config.get("JSON_PATH")
+        args.spixel_path = config.get("SPIXEL_PATH")
+
+        args.scoring_function = SCORING_FUNCTION_MAP.get(config.get("scoring_function"))
+        args.pruning_function = PRUNING_FUNCTION_MAP.get(config.get("pruning_function"))
+        args.batch_size = config.get("batch_size")
         args.scoring_function("")
         args.pruning_function("")
-    
-    main(args) 
-    
+
+    main(args)
---
+++ testbed/scoring_sampling.py
@@ -0,0 +1,69 @@
+import torch
+from typing import Tuple, Callable
+
+
+def random_bipartite_soft_matching(
+    metric: torch.Tensor, r: int = 30
+) -> Tuple[Callable, Callable]:
+    """
+    Applies ToMe with the two sets as (r chosen randomly, the rest).
+    Input size is [batch, tokens, channels].
+
+    This will reduce the number of tokens by r.
+    """
+    with torch.no_grad():
+        B, N, _ = metric.shape
+        rand_idx = torch.rand(B, N, 1, device=metric.device).argsort(dim=1)
+
+        a_idx = rand_idx[:, :r, :]
+        b_idx = rand_idx[:, r:, :]
+
+        def split(x):
+            C = x.shape[-1]
+            a = x.gather(dim=1, index=a_idx.expand(B, r, C))
+            b = x.gather(dim=1, index=b_idx.expand(B, N - r, C))
+            return a, b
+
+        metric = metric / metric.norm(dim=-1, keepdim=True)
+        a, b = split(metric)
+        scores = a @ b.transpose(-1, -2)
+
+        _, dst_idx = scores.max(dim=-1)
+        dst_idx = dst_idx[..., None]
+
+    def merge(x: torch.Tensor, mode="mean") -> torch.Tensor:
+        src, dst = split(x)
+        C = src.shape[-1]
+        dst = dst.scatter_reduce(-2, dst_idx.expand(B, r, C), src, reduce=mode)
+
+        return dst
+
+    def unmerge(x: torch.Tensor) -> torch.Tensor:
+        C = x.shape[-1]
+        dst = x
+        src = dst.gather(dim=-2, index=dst_idx.expand(B, r, C))
+
+        out = torch.zeros(B, N, C, device=x.device, dtype=x.dtype)
+
+        out.scatter_(dim=-2, index=a_idx.expand(B, r, C), src=src)
+        out.scatter_(dim=-2, index=b_idx.expand(B, N - r, C), src=dst)
+
+        return out
+
+    return merge, unmerge
+
+
+def sampling_method_(tensor):
+    print("Function: Test reduce")
+    tensor_temp = tensor.unsqueeze(0)
+    merge, unmerge = random_bipartite_soft_matching(tensor_temp)
+
+    # Now you can use `merge` and `unmerge` functions on your input
+    merged = merge(tensor_temp)  # Merge operation
+    unmerged = unmerge(merged)  # Unmerge operation
+
+    # tensor = merged.squeeze(0)
+
+    print("Merged tensor shape:", merged.shape)
+    print("Unmerged tensor shape:", unmerged.shape)
+    return merged.squeeze(0)
